19fb1cc8cce17ec618eeee2ada148971d2db8f4e
Hugo Ludmann <ludmann.h@gmail.com>
2021-12-04 22:17:05 +0000

    day4

---
+++ lib/day4.ex
@@ -0,0 +1,125 @@
+defmodule AdventOfCode2021.Day4 do
+  def puzzle1() do
+    {num_list, parsed_tables} =
+      AdventOfCode2021.get_input(4)
+      |> parse_input()
+
+    num_list
+    |> Enum.reduce_while(parsed_tables, fn num, tables ->
+      updated = Enum.map(tables, &check_num(num, &1))
+
+      rem_sum =
+        Enum.reduce_while(updated, -1, fn table, _ ->
+          case has_won?(table) do
+            false -> {:cont, -1}
+            true -> {:halt, sum_remaining(table)}
+          end
+        end)
+
+      case rem_sum do
+        -1 -> {:cont, updated}
+        sum -> {:halt, sum * num}
+      end
+    end)
+  end
+
+  def puzzle2() do
+    {num_list, parsed_tables} =
+      AdventOfCode2021.get_input(4)
+      |> parse_input()
+
+    num_list
+    |> Enum.reduce_while(parsed_tables, fn num, tables ->
+      case Enum.map(tables, &check_num(num, &1)) do
+        [last] ->
+          case has_won?(last) do
+            true -> {:halt, sum_remaining(last)* num}
+            false -> {:cont, [last]}
+          end
+
+        updated ->
+          {:cont, updated |> Enum.filter(&(not has_won?(&1)))}
+      end
+    end)
+  end
+
+  defp parse_input(input) do
+    [num_chain | tables] = input
+
+    numbers = num_chain |> String.split(",", trim: true) |> Enum.map(&String.to_integer/1)
+
+    formated_tables =
+      tables
+      |> Stream.chunk_every(5, 5, :discard)
+      |> Stream.map(fn raw_table ->
+        raw_table
+        |> Enum.map(fn raw_line ->
+          raw_line
+          |> String.split(" ", trim: true)
+          |> Enum.map(&String.to_integer/1)
+        end)
+      end)
+      |> Enum.to_list()
+
+    {numbers, formated_tables}
+  end
+
+  defp check_num(num, table) do
+    table
+    |> Enum.map(fn row ->
+      Enum.map(row, fn
+        ^num -> -1
+        e -> e
+      end)
+    end)
+  end
+
+  defp has_won?(table)
+
+  defp has_won?([[-1, -1, -1, -1, -1] | _]), do: true
+  defp has_won?([_, [-1, -1, -1, -1, -1] | _]), do: true
+  defp has_won?([_, _, [-1, -1, -1, -1, -1] | _]), do: true
+  defp has_won?([_, _, _, [-1, -1, -1, -1, -1] | _]), do: true
+  defp has_won?([_, _, _, _, [-1, -1, -1, -1, -1]]), do: true
+  defp has_won?([[-1 | _], [-1 | _], [-1 | _], [-1 | _], [-1 | _]]), do: true
+  defp has_won?([[_, -1 | _], [_, -1 | _], [_, -1 | _], [_, -1 | _], [_, -1 | _]]), do: true
+
+  defp has_won?([[_, _, -1 | _], [_, _, -1 | _], [_, _, -1 | _], [_, _, -1 | _], [_, _, -1 | _]]),
+    do: true
+
+  defp has_won?([
+         [_, _, _, -1 | _],
+         [_, _, _, -1 | _],
+         [_, _, _, -1 | _],
+         [_, _, _, -1 | _],
+         [_, _, _, -1 | _]
+       ]),
+       do: true
+
+  defp has_won?([
+         [_, _, _, _, -1, _],
+         [_, _, _, _, -1, _],
+         [_, _, _, _, -1, _],
+         [_, _, _, _, -1, _],
+         [_, _, _, _, -1, _]
+       ]),
+       do: true
+
+  defp has_won?([
+         [_, _, _, _, -1],
+         [_, _, _, _, -1],
+         [_, _, _, _, -1],
+         [_, _, _, _, -1],
+         [_, _, _, _, -1]
+       ]),
+       do: true
+
+  defp has_won?(_), do: false
+
+  defp sum_remaining(table) do
+    Enum.map(table, fn row ->
+      row |> Enum.filter(&(&1 != -1)) |> Enum.sum()
+    end)
+    |> Enum.sum()
+  end
+end
