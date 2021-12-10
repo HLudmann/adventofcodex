3cf2dfda66e3da9b5e678576893928237a3fd4ff
Hugo Ludmann <ludmann.h@gmail.com>
2021-12-10 21:19:24 +0000

    day10

--- lib/advent_of_code2021.ex
+++ lib/advent_of_code2021.ex
@@ -3,12 +3,14 @@ defmodule AdventOfCode2021 do
   Documentation for `AdventOfCode2021`.
   """
 
-  @days_solved 9
+  @days_solved 10
 
+  @spec get_input(number) :: [String.t]
   def get_input(day) do
     File.read!("puzzle_inputs/day#{day}.txt") |> String.split("\n", trim: true)
   end
 
+  @spec get_input_as_integer(number) :: [integer]
   def get_input_as_integer(day), do: get_input(day) |> Enum.map(&String.to_integer/1)
 
   1..@days_solved
@@ -19,10 +21,12 @@ defmodule AdventOfCode2021 do
     end
   end)
 
+  @spec all_days :: :ok
   def all_days() do
     for day <- 1..@days_solved do
       module = String.to_existing_atom("Elixir.AdventOfCode2021.Day#{day}")
       IO.puts("Day#{day}:\n\tpuzzle1: #{module.puzzle1()}\n\tpuzzle2: #{module.puzzle2()}")
     end
+    :ok
   end
 end
