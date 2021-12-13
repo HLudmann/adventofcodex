df01072fbdc71d1f24ab036597a289048c0408ab
Hugo Ludmann <ludmann.h@gmail.com>
2021-12-13 18:14:44 +0000

    day13

--- lib/advent_of_code2021.ex
+++ lib/advent_of_code2021.ex
@@ -3,7 +3,7 @@ defmodule AdventOfCode2021 do
   Documentation for `AdventOfCode2021`.
   """
 
-  @days_solved 12
+  @days_solved 13
 
   @spec get_input(number) :: [String.t]
   def get_input(day) do
